e784e81e4fc793ebf56d847609ac51206fcb9540
Alejandro <alex@gomez.com>
2021-05-12 12:27:54 +0200

    Pintor

---
+++ EjemploGit/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"prueba","c":"Prueba1","l":"nombreMetodo(int, boolean)","url":"nombreMetodo(int,boolean)"},{"p":"prueba","c":"Prueba1","l":"Prueba1()","url":"%3Cinit%3E()"}]
---
+++ EjemploGit/src/prueba/Prueba1.java
@@ -0,0 +1,17 @@
+package prueba;
+/**
+ * 
+ * @author Alejandro
+ * @version 1.1
+ * @since 2021-05-07
+ */
+public class Prueba1 {
+	
+	public boolean nombreMetodo (int var1, boolean var2) {
+		
+	return false;
+	
+	System.out.println("Pablo Picasso");
+	
+	}
+}
